4ed90b035a5627026859515648f0e304132825a4
Jay Patel <jaypatel27694@gmail.com>
2024-01-08 15:33:27 -0600

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,7 +2,11 @@
 
 export async function handleSubmit(state: any, formData: FormData) {   
 
+    console.log("inside handleSubmit")
+
     const text = formData.get("text");
+
+    console.log("text: " + text)
 
     const res = await fetch("https://webhooks.workato.com/webhooks/rest/d1128161-05ee-4c1b-b929-f5e9365b055f/webhook-translator", {
       method: "POST",
@@ -11,6 +15,8 @@ export async function handleSubmit(state: any, formData: FormData) {
       },
       body: JSON.stringify({ "text":  text}),
     });
+
+    console.log(res)
     
     if(res.status === 200) return true
 
